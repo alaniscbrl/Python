c64d7408dba1bdc3a117fc36dd08429efc761749
Alanis <alanisgabrielle30@gmail.com>
2024-08-28 17:17:12 -0300

    Add files via upload

---
+++ 01-aula01.py
@@ -0,0 +1,6 @@
+#Aqui eu posso adicionar comentários
+#Comentários na linha de baixo
+print("Alanis") # comentário a linha do código
+print('Alanis')
+print('Alanis "Lanis" Gabrielle')
+print('Alanis "Lanis" Gabrielle')6
