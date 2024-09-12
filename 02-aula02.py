0891e243bec27cfc54785399c067cdc44f6e4fe2
Alanis <alanisgabrielle30@gmail.com>
2024-09-12 16:26:01 -0300

    Add files via upload

---
+++ 02-aula02.py
@@ -0,0 +1,9 @@
+# atribuição de variáveis
+nome = "Alanis"
+idade = 16
+altura = 1.65
+
+# exibir mensagem na tela
+print(" nome: ", nome, "idade ", idade, "altura: ", altura)
+# formção com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
